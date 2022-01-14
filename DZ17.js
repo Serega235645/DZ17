04130a478edec47506b9b07b9337975a2062de4f
Serega235645 <87755400+Serega235645@users.noreply.github.com>
2022-01-14 22:07:49 +0200

    Add files via upload

---
+++ DZ17.js
@@ -0,0 +1,18 @@
+
+        var a = prompt ("Your number");
+        parseInt(a)
+        if(a  >=  1 && a <= 5) {
+            alert("Yes")
+        }
+        else{
+            alert("No")
+        }    
+        
+        if(a >= 1){
+            if(a <= 5){
+                alert("Correct")
+            }
+        }
+        else{  alert("Correct")
+
+        }
